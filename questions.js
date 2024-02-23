3166092b1c365e055befbe257cee6c8690b78245
Isil <160738229+isil-isil@users.noreply.github.com>
2024-02-23 15:16:54 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Quel est le véritable nom de Lord Voldemort ?",
+    answer: "C. Tom Jedusor",
+    options: [
+      "A. Severus Rogue",
+      "B. Albus Dumbledore",
+      "C. Tom Jedusor",
+      "D. Sirius Black",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Dans quelle maison à Poudlard Harry Potter a-t-il atteri ?",
+    answer: "A. Gryffondor",
+    options: [
+      "A. Gryffondor",
+      "B. Serpentard",
+      "C. Poufsouffle",
+      "D. Serdaigle",
+    ],
+  },
+  {
+    numb: 3,
+    question: "Quel est le sortilège utilisé pour immobiliser une personne ?",
+    answer: "A. Petrificus Totalus",
+    options: [
+      "A. Petrificus Totalus",
+      "B. Expelliarmus",
+      "C. Avada Kedavra",
+      "D. Sectumsempra",
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      "Comment s'appelle le premier tome de la série Harry Potter en version originale ?",
+    answer: "D. Harry Potter and the Philosopher's Stone",
+    options: [
+      "A. Harry Potter and the Chamber of Secrets",
+      "B. Harry Potter and the Sorcerer's Stone",
+      "C. Harry Potter and the Prisoner of Azkaban",
+      "D. Harry Potter and the Philosopher's Stone",
+    ],
+  },
+  {
+    numb: 5,
+    question:
+      "Quel est le nom du fantôme résidant dans la salle commune de Gryffondor ?",
+    answer: "D. Nick Quasi-Sans-Tête",
+    options: [
+      "A. La Dame Grise",
+      "B. Le Baron Sanglant",
+      "C. Peeves",
+      "D. Nick Quasi-Sans-Tête",
+    ],
+  },
+];
